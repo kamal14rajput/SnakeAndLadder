f648b8c4750f82b08f7c7d3e78bd5e549576cb85
kamal14rajput <kamal14rajput@gmail.com>
2021-12-27 17:43:55 +0530

    ADDED: random number from 1 to 6

--- src/com/bridgelabz/SnakeAndLadderMain.java
+++ src/com/bridgelabz/SnakeAndLadderMain.java
@@ -4,7 +4,10 @@ public class SnakeAndLadderMain {
 
 	public static void main(String[] args) {
 		int START_POSITION = 0;
-		System.out.println(START_POSITION);
+		System.out.println("Starting Position is:" + START_POSITION);
+
+		int ROLL_DICE = (int) (Math.floor(Math.random() * 10 ) % 6) + 1 ;
+		System.out.println("After Rolling Dice We Get:" + ROLL_DICE);
 	}
 
 }
