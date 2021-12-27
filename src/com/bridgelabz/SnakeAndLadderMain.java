fc912f7cdfcf973643a088725e99613008455737
kamal14rajput <kamal14rajput@gmail.com>
2021-12-27 18:04:01 +0530

    ADDED: prints exact winning of 100

--- src/com/bridgelabz/SnakeAndLadderMain.java
+++ src/com/bridgelabz/SnakeAndLadderMain.java
@@ -3,18 +3,20 @@ package com.bridgelabz;
 public class SnakeAndLadderMain {
 
 	public static void main(String[] args) {
+
 		int START_POSITION = 0;
 		final int NO_PLAY=1;
         final int LADDER=2;
         final int SNAKE=3;
         final int WIN=100;
         int playerPosition=0;
+        int DICE_PLAYED=0;
         
 		System.out.println("Starting Position is ::"+START_POSITION);
 
-		while( playerPosition <= WIN) 
+		while( playerPosition < WIN) 
 		{
-			
+			DICE_PLAYED++;
 		
 			int ROLL_DICE = (int) (Math.floor(Math.random() * 10 ) % 6) + 1 ;
 			System.out.println("After Rolling Dice We Get ::"+ROLL_DICE);
@@ -26,16 +28,22 @@ public class SnakeAndLadderMain {
 	        switch(playerCheckOption)
 	        {
 	            case NO_PLAY:
+	            	
 	            	System.out.println("It's No Play, Your position is not changed, You are at :"+playerPosition);                
 	            	break;
 	         
 	            case LADDER:
-	                playerPosition = playerPosition + ROLL_DICE;
-	                System.out.println("Congratulations !! You Got the ladder your position will be increesed by "+ROLL_DICE);
+	            	
+	            	if((playerPosition + ROLL_DICE) <= WIN)
+	            	{
+		            	playerPosition = playerPosition + ROLL_DICE;
+		                System.out.println("Congratulations !! You Got the ladder your position will be increesed by "+ROLL_DICE);
+	            	}
 	                break;
 	            
 	            case SNAKE:
-	            	if((playerPosition-ROLL_DICE)<START_POSITION)
+	            	
+	            	if((playerPosition - ROLL_DICE) <= START_POSITION)
 	            	{
 		            	playerPosition = START_POSITION;
 	            	}
@@ -52,5 +60,6 @@ public class SnakeAndLadderMain {
 	        }
 	        System.out.println("Position :: "+playerPosition);
 		}
+		System.out.println("We just make "+DICE_PLAYED+" Number of dice to win the game");
 	}
 }
